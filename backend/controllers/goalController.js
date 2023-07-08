d3d93372711f05d5bcb9272d7f43868c579f40b7
diamond <diamondyucas@gmail.com>
2023-07-08 14:42:52 -0500

    Accept Body Data

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -7,6 +7,14 @@ const getGoals = (req, res) => {
 }
 
 const setGoals = (req, res) => {
+    if(!req.body.text) {
+        res.status(400)
+        throw new Error('Please add a text field')
+    }
+
+
+    console.log(req.body)
+
     res.status(200).json({message: 'Set Goals'})
 }
 
