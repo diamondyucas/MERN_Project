3bfeb497c1971837a4ca5c232bc1d0e1131a1213
diamond <diamondyucas@gmail.com>
2023-07-08 14:00:01 -0500

    First Route

--- backend/server.js
+++ backend/server.js
@@ -4,5 +4,9 @@ const port = process.env.PORT || 3000
 
 const app = express()
 
+app.get('/api/goals', (req, res) => {
+    res.status(200).json({message: 'Get Goals'})
+})
+
 app.listen(port, () => console.log(`Server started on port ${port}`))
 
